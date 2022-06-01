b882aa61c8a79ff881fadbdc5b7eaa7f788cb0f3
Dpons039 <dpons@outlook.com>
2022-06-01 18:22:24 +0200

    Add useState

--- src/components/UserRole.jsx
+++ src/components/UserRole.jsx
@@ -1,10 +1,12 @@
 import style from './UserRole.module.css';
+
+const ROLE_STYLES = {
+	teacher: ['Profesor', style.teacher],
+	student: ['Alumno', style.student],
+	other: ['Otro', style.other]
+};
+
 const UserRole = ({ role }) => {
-	const ROLE_STYLES = {
-		teacher: ['Profesor', style.teacher],
-		student: ['Alumno', style.student],
-		other: ['Otro', style.other]
-	};
 	const [roleName, roleClassName] = ROLE_STYLES[role] || ROLE_STYLES.other;
 	return <span className={`${roleClassName} ${style.role}`}>{roleName}</span>;
 };
