e6e4aad962c3945e2176b6c8efbe9b4003979651
Dpons039 <dpons@outlook.com>
2022-06-02 01:17:38 +0200

    Users list with custom hooks

--- src/app.jsx
+++ src/app.jsx
@@ -2,31 +2,34 @@ import UsersList from './components/UsersList';
 
 const USERS = [
 	{
+		id: 0,
 		name: 'Pablo Castellanos',
 		active: true,
 		role: 'teacher'
 	},
 	{
+		id: 1,
 		name: 'Jose Miguel Fernández',
 		active: true,
 		role: 'teacher'
 	},
 	{
+		id: 2,
 		name: 'David Pons',
 		active: true,
 		role: 'student'
 	},
 	{
+		id: 3,
 		name: 'Javier López',
 		active: false,
 		role: 'other'
 	}
 ];
 
+
 const App = () => (
-	<UsersList users={USERS}>
-		<h1>Listado de usuarios</h1>
-	</UsersList>
+	<UsersList initialUsers={USERS} />
 );
 
 export default App;
